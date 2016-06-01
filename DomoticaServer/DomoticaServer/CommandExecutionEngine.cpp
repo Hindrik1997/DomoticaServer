10c2e3856d2862aa8bdd7e8a213a07721841ca0e
Hindrik Stegenga <Hindrik Stegenga>
2016-06-01 18:27:45 +0200

    Added a little bit to the command processing

--- DomoticaServer/DomoticaServer/CommandExecutionEngine.cpp
+++ DomoticaServer/DomoticaServer/CommandExecutionEngine.cpp
@@ -1,8 +1,7 @@
 #include "CommandExecutionEngine.h"
+#include "Common.h"
 
-
-
-CommandExecutionEngine::CommandExecutionEngine(size_t threadcount) : pool(threadcount - 1)
+CommandExecutionEngine::CommandExecutionEngine(size_t threadcount, Connection& con) : pool(threadcount - 1), conref(con)
 {
 }
 
@@ -13,5 +12,37 @@ CommandExecutionEngine::~CommandExecutionEngine()
 
 void CommandExecutionEngine::Execute(string command)
 {
+	//Commands!
+	if (command[0] == 'R' && command[1] == 'F' && command[2] == 'I' && command[3] == 'D' && command[4] == ':' && command[5] == ' ')
+	{
+		string RFID;
+		//RFID TAG!
+		for (int i = 6; i < command.length(); ++i)
+		{
+			RFID += command[i];
+		}
+		Query query = conref.query();
+		query << "SELECT * FROM RFID";
+		StoreQueryResult res = query.store();
+		for (size_t i = 0; i < res.num_rows(); ++i)
+		{
+			cout << "Name: " << res[i]["RFID_CODE"] << " " << res[i]["CHECKED_IN"] << " " << std::endl;			
+		}
+		
+		
+		
+		
+		
+	}
+	
+	if(command[0] == 'K' && command[1] == 'A')
+	{
+		cout << "Koffie apparaat aan!" << endl;		
+	}
+	
+	if (command[0] == 'K' && command[1] == 'Z')
+	{
+		cout << "Koffie zetten!" << endl;		
+	}
 	
 }
--- DomoticaServer/DomoticaServer/CommandExecutionEngine.h
+++ DomoticaServer/DomoticaServer/CommandExecutionEngine.h
@@ -3,13 +3,17 @@
 #include "Threadpool.h"
 #include "Task.h"
 
+using namespace mysqlpp;
+
+
 class CommandExecutionEngine
 {
 public:
-	CommandExecutionEngine(size_t threadcount);
+	CommandExecutionEngine(size_t threadcount, Connection& con);
 	~CommandExecutionEngine();
 	void Execute(string command);
 private:
+	Connection& conref;
 	Threadpool pool;
 };
 
